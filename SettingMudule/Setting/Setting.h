ffcc477246a2c2fe491cd40dd2fa6eb680ccb3fb
codepgq <devpgq@126.com>
2019-05-14 10:52:32 +0800

    移除第三方依赖

---
+++ SettingMudule/Setting/Setting.h
@@ -0,0 +1,19 @@
+//
+//  Setting.h
+//  Setting
+//
+//  Created by 盘国权 on 2019/5/14.
+//  Copyright © 2019 pgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Setting.
+FOUNDATION_EXPORT double SettingVersionNumber;
+
+//! Project version string for Setting.
+FOUNDATION_EXPORT const unsigned char SettingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Setting/PublicHeader.h>
+
+
---
+++ SettingMudule/Setting/SettingController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingController.swift
+//  Setting
+//
+//  Created by 盘国权 on 2019/5/14.
+//  Copyright © 2019 pgq. All rights reserved.
+//
+
+import UIKit
+
+open class SettingController: UIViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .orange
+    }
+    
+
+}
